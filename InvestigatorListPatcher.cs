1604b01a9e74cdda02e14f1cd553f6d41b4b9ff4
gwisp2 <60121533+gwisp2@users.noreply.github.com>
2024-09-21 00:56:37 +0300

    Initial commit: and it works

---
+++ InvestigatorListPatcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+
+namespace InvestigatorUnlocker;
+
+[HarmonyPatch(typeof(MoMDatabase))]
+public class InvestigatorListPatcher
+{
+    [HarmonyPrefix]
+    [HarmonyPatch("GetAvailableInvestigators")]
+    static bool PreGetAvailableInvestigators(ref IEnumerable<InvestigatorModel> __result)
+    {
+        var hashSet = new HashSet<InvestigatorModel>();
+        var productCollection = UserCollectionManager.GetProductCollection();
+        foreach (var productModel in productCollection.Keys)
+        {
+            hashSet.UnionWith(productModel.Investigators);
+        }
+        __result = hashSet.OrderBy(i => Localization.Get(i.Name.Key));
+        return false;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace InvestigatorUnlocker
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger = base.Logger;
+            Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} is loaded!");
+            // Patch the methods
+            var harmony = new Harmony("InvestigatorUnlocker");
+            harmony.PatchAll();
+        }
+    }
+}
